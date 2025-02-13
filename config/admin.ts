336d76326a6c27f336157389c3e235d6b0cc11b7
pawan bhandari <42120861+Pawan376@users.noreply.github.com>
2025-02-13 18:29:26 +0530

    Update admin.ts

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'bYIYdkww/iQuHd+vaeFnfw=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'VDJ2qzGw7DI1XdSSU8N7Gg=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'teMNUkRSzYhj4ahqCmhs9w=='),
     },
   },
   flags: {
